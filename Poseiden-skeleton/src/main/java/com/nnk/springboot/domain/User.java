85b6000a9d2fd627c81b01c9ee9be0df2563d7bc
jedi10 <kyle10_dev@hotmail.fr>
2020-11-30 18:01:53 +0100

    Update User Pojo: Lombok for getter/setter/noArgsConstructor

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
@@ -1,60 +1,40 @@
 package com.nnk.springboot.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Getter
+    @Setter
     private Integer id;
+
     @NotBlank(message = "Username is mandatory")
+    @Getter
+    @Setter
     private String username;
+
     @NotBlank(message = "Password is mandatory")
+    @Getter
+    @Setter
     private String password;
+
     @NotBlank(message = "FullName is mandatory")
+    @Getter
+    @Setter
     private String fullname;
+
     @NotBlank(message = "Role is mandatory")
+    @Getter
+    @Setter
     private String role;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
