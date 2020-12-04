a0602fee830b602d515d089df0343bd7784f4d36
jedi10 <kyle10_dev@hotmail.fr>
2020-12-04 20:47:38 +0100

    Synchronize validation fields and DBB constraint

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -18,30 +20,37 @@ public class User {
     private Integer id;
 
     @NotBlank(message = "Username is mandatory")
-    @Column(nullable = false, length = 35)
+    @NotNull(message = "UserName is mandatory")
+    @Size(min = 2, max = 35, message = "Name must be between 2 and 35 characters long")
+    //@Column(nullable = false, length = 35)
     @Getter
     @Setter
     private String username;
 
     @NotBlank(message = "Password is mandatory")
-    @Column(nullable = false)
+    @NotNull(message = "Password is mandatory")
+    //@Column(nullable = false)
     @Getter
     @Setter
     private String password;
 
     @NotBlank(message = "FullName is mandatory")
-    @Column(nullable = false, length = 35)
+    @NotNull(message = "FullName is mandatory")
+    @Size(min = 2, max = 35, message = "FullName must be between 2 and 35 characters long")
+    //@Column(nullable = false, length = 35)
     @Getter
     @Setter
     private String fullname;
 
     @NotBlank(message = "Role is mandatory")
-    @Column(nullable = false, length = 35)
+    @NotNull(message = "Role is mandatory")
+    @Size(min = 2, max = 35, message = "Role must be between 2 and 35 characters long")
+    //@Column(nullable = false, length = 35)
     @Getter
     @Setter
     private String role;
 
 }
 
-
+//https://www.baeldung.com/java-bean-validation-not-null-empty-blank
 //https://www.baeldung.com/hibernate-notnull-vs-nullable
