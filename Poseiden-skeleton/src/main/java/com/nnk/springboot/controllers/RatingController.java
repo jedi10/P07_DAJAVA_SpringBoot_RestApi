2b891ba7d9753cd746cd53ad4989d274009437ff
jedi10 <kyle10_dev@hotmail.fr>
2020-12-21 20:45:40 +0100

    Add Rating Controller; Unit Test; Fix HTML page

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,54 +1,84 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/rating/")
 public class RatingController {
-    // TODO: Inject Rating service
 
-    @RequestMapping("/rating/list")
+    private RatingRepository ratingRepository;
+
+    public RatingController(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
     public String home(Model model)
     {
-        // TODO: find all Rating, add to model
+        model.addAttribute("ratings", ratingRepository.findAll());
         return "rating/list";
     }
 
-    @GetMapping("/rating/add")
-    public String addRatingForm(Rating rating) {
+    @GetMapping(value = "add")
+    public String addRatingForm(Rating rating)
+    {
         return "rating/add";
     }
 
-    @PostMapping("/rating/validate")
-    public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Rating list
-        return "rating/add";
+    @PostMapping(value = "validate")
+    public String validate(@RequestBody @Valid Rating rating,
+                           BindingResult result,
+                           Model model) {
+        if(result.hasErrors()){
+            return "rating/add";
+        }
+        ratingRepository.save(rating);
+        model.addAttribute("ratings", ratingRepository.findAll());
+        return "rating/list";
     }
 
-    @GetMapping("/rating/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Rating by Id and to model then show to the form
+    @GetMapping(value= "update/{id}")
+    public String showUpdateForm(@PathVariable("id") Integer id,
+                                 Model model) {
+        String errorMessage = "Invalid rating Id: there is no rating with Id: ";
+        Rating rating = ratingRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException(errorMessage + id));
+        model.addAttribute("rating", rating);
         return "rating/update";
     }
 
-    @PostMapping("/rating/update/{id}")
-    public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Rating and return Rating list
-        return "redirect:/rating/list";
+    @PutMapping(value = "update/{id}")
+    public String updateRating(@PathVariable("id") Integer id,
+                               @RequestBody @Valid Rating rating,
+                               BindingResult result,
+                               Model model) {
+        if(result.hasErrors()){
+            return "rating/update";
+        }
+        rating.setId(id);
+        ratingRepository.save(rating);
+        model.addAttribute("ratings", ratingRepository.findAll());
+        return "rating/list";
     }
 
-    @GetMapping("/rating/delete/{id}")
+    @RolesAllowed("ADMIN")
+    @DeleteMapping("delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Rating by Id and delete the Rating, return to Rating list
-        return "redirect:/rating/list";
+        Optional<Rating> rating = ratingRepository.findById(id);
+        if(rating.isPresent()){
+            ratingRepository.deleteById(id);
+        }
+        model.addAttribute("ratings", ratingRepository.findAll());
+        return "rating/list";
     }
 }
