5783d00ab9a29defe1b45624c0b21d26bec3f394
jedi10 <kyle10_dev@hotmail.fr>
2020-12-22 14:41:31 +0100

    Add RuleName Controller; Unit Test; Fix HTML page

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
 @Table(name = "rulename")
@@ -19,6 +20,7 @@ public class RuleName {
     @Setter
     private Integer id;
 
+    @NotBlank(message = "name is mandatory")
     @Getter
     @Setter
     private String name;
@@ -65,5 +67,35 @@ public class RuleName {
         this.template = template;
         this.sqlStr = sqlStr;
         this.sqlPart = sqlPart;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RuleName)) return false;
+        RuleName ruleName = (RuleName) o;
+        return Objects.equals(name, ruleName.name) &&
+                Objects.equals(description, ruleName.description) &&
+                Objects.equals(json, ruleName.json) &&
+                Objects.equals(template, ruleName.template) &&
+                Objects.equals(sqlStr, ruleName.sqlStr) &&
+                Objects.equals(sqlPart, ruleName.sqlPart);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, json, template, sqlStr, sqlPart);
+    }
+
+    @Override
+    public String toString() {
+        return "RuleName{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", json='" + json + '\'' +
+                ", template='" + template + '\'' +
+                ", sqlStr='" + sqlStr + '\'' +
+                ", sqlPart='" + sqlPart + '\'' +
+                '}';
     }
 }
