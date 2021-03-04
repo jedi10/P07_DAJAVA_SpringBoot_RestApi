3499ddd4d7b2989b86179d47e5563336f8c8b66e
jedi10 <kyle10_dev@hotmail.fr>
2021-03-04 15:02:43 +0100

    Add User Dal Service; Add User Repository method; Add UserDTO
    User Repository is now used by a service: this service is mandatory because of 2 Spring security methods that need to be implemented: loadUserByUsername(), create(): this last method use BCryptPasswordEncoder from pringSecurity to encode password;

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/IUserDalService.java
@@ -0,0 +1,19 @@
+package com.nnk.springboot.service;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.web.dto.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Collection;
+
+public interface IUserDalService extends UserDetailsService {
+
+    User create(UserDTO user);
+    Collection<User> findAll();
+    User findOne(Integer id);
+    User findByName(String name);
+    User update(User user);
+    void delete(Integer id);
+    void deleteAll();
+
+}
