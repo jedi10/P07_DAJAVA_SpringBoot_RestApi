9061feef96b8db941448508224089a99a5c55b5e
jedi10 <kyle10_dev@hotmail.fr>
2021-03-15 17:00:05 +0100

    Add library Passay for User Password Validation
    @ValidPassword annotation is now available; All User Errors are now showed on Creation and Update

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/web/dto/UserDTO.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/web/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.nnk.springboot.web.dto;
 
+import com.nnk.springboot.web.dto.validation.ValidPassword;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
@@ -19,21 +20,18 @@ public class UserDTO {
     private Integer id;
 
     @NotBlank(message = "Username is mandatory")
-    @NotNull(message = "UserName is mandatory")
-    @Size(min = 2, max = 35, message = "Name must be between 2 and 35 characters long")
+    @Size(min = 2, max = 35, message = "UserName must be between 2 and 35 characters long")
     private String username;
 
     @NotBlank(message = "Password is mandatory")
-    @NotNull(message = "Password is mandatory")
+    @ValidPassword
     private String password;
 
     @NotBlank(message = "FullName is mandatory")
-    @NotNull(message = "FullName is mandatory")
     @Size(min = 2, max = 35, message = "FullName must be between 2 and 35 characters long")
     private String fullname;
 
     @NotBlank(message = "Role is mandatory")
-    @NotNull(message = "Role is mandatory")
     @Size(min = 2, max = 35, message = "Role must be between 2 and 35 characters long")
     private String role;
 
