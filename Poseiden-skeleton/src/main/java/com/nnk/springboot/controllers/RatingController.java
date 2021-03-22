342e4425df4a7bc35bba9dbf90bd4c8a9aae7ba4
jedi10 <kyle10_dev@hotmail.fr>
2021-03-22 20:07:00 +0100

    Add Logger in RatingController

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,37 +1,52 @@
 package com.nnk.springboot.controllers;
 
-import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.domain.Rating;
 import com.nnk.springboot.repositories.RatingRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 @RequestMapping("/rating/")
 public class RatingController {
 
     private RatingRepository ratingRepository;
 
+    private String dataName = "Rating";
+
     public RatingController(RatingRepository ratingRepository) {
         this.ratingRepository = ratingRepository;
     }
 
     @RequestMapping(value = "list", method = RequestMethod.GET)
-    public String home(Model model)
+    public String home(Model model, HttpServletRequest request, HttpServletResponse response)
     {
+        log.info("Get {} List on URI: '{}' : RESPONSE STATUS: '{}'",
+                dataName,
+                request.getRequestURI(),
+                response.getStatus());
         model.addAttribute("ratings", ratingRepository.findAll());
         return "rating/list";
     }
 
     @GetMapping(value = "add")
-    public String addRatingForm(Rating rating, Model model)
+    public String addRatingForm(Rating rating, Model model,
+                                HttpServletRequest request, HttpServletResponse response)
     {
+        log.info("Go to {} Creation Form on URI: '{}' : RESPONSE STATUS: '{}'",
+                dataName,
+                request.getRequestURI(),
+                response.getStatus());
         model.addAttribute("rating", new Rating());
         return "rating/add";
     }
@@ -39,21 +54,41 @@ public class RatingController {
     @PostMapping(value = "validate")
     public String validate(@Valid @ModelAttribute("rating") Rating rating,
                            BindingResult result,
-                           Model model) {
+                           Model model,
+                           HttpServletRequest request, HttpServletResponse response) {
         if(result.hasErrors()){
+            log.warn("{} Creation Error on URI: '{}': Error Field(s): '{}' : RESPONSE STATUS: '{}'",
+                    dataName,
+                    request.getRequestURI(),
+                    result.getFieldErrors().stream()
+                            .map(e-> e.getField().toUpperCase())
+                            .distinct()
+                            .collect(Collectors.joining(", ")),
+                    response.getStatus());
             return "rating/add";
         }
-        ratingRepository.save(rating);
+        Rating ratingCreated = ratingRepository.save(rating);
+        log.info("{} Creation on URI: '{}' : {} Created '{}' : RESPONSE STATUS: '{}'",
+                dataName,
+                request.getRequestURI(),
+                dataName,
+                ratingCreated.getId() + " " + ratingCreated.getFitchRating(),
+                response.getStatus());
         model.addAttribute("ratings", ratingRepository.findAll());
         return "rating/list";
     }
 
     @GetMapping(value= "update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id,
-                                 Model model) {
+                                 Model model,
+                                 HttpServletRequest request, HttpServletResponse response) {
         String errorMessage = "Invalid rating Id: there is no rating with Id: ";
         Rating rating = ratingRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException(errorMessage + id));
+        log.info("Go to {} Update Form on URI: '{}': RESPONSE STATUS: '{}'",
+                dataName,
+                request.getRequestURI(),
+                response.getStatus());
         model.addAttribute("rating", rating);
         return "rating/update";
     }
@@ -62,22 +97,47 @@ public class RatingController {
     public String updateRating(@PathVariable("id") Integer id,
                                @Valid @ModelAttribute("rating") Rating rating,
                                BindingResult result,
-                               Model model) {
+                               Model model,
+                               HttpServletRequest request, HttpServletResponse response) {
         if(result.hasErrors()){
+            log.warn("{} Update Error on URI: '{}': Error Field(s):'{}' : RESPONSE STATUS: '{}'",
+                    dataName,
+                    request.getRequestURI(),
+                    result.getFieldErrors().stream()
+                            .map(e-> e.getField().toUpperCase())
+                            .distinct()
+                            .collect(Collectors.joining(", ")),
+                    response.getStatus());
             return "rating/update";
         }
         rating.setId(id);
-        ratingRepository.save(rating);
+        Rating ratingUpdated = ratingRepository.save(rating);
+        log.info("{} Update on URI: '{}' : {} Updated '{}' : RESPONSE STATUS: '{}'",
+                dataName,
+                request.getRequestURI(),
+                dataName,
+                ratingUpdated.getId() + " " + ratingUpdated.getFitchRating(),
+                response.getStatus());
         model.addAttribute("ratings", ratingRepository.findAll());
         return "rating/list";
     }
 
     @RolesAllowed("ADMIN")
     @GetMapping(value ="delete/{id}")
-    public String deleteRating(@PathVariable("id") Integer id, Model model) {
+    public String deleteRating(@PathVariable("id") Integer id, Model model,
+                               HttpServletRequest request, HttpServletResponse response) {
         Optional<Rating> rating = ratingRepository.findById(id);
         if(rating.isPresent()){
             ratingRepository.deleteById(id);
+            log.info("Delete {} on URI: '{}' : RESPONSE STATUS: '{}'",
+                    dataName,
+                    request.getRequestURI(),
+                    response.getStatus());
+        } else {
+            log.warn("No {} was deleted on URI: '{}' : RESPONSE STATUS: '{}'",
+                    dataName,
+                    request.getRequestURI(),
+                    response.getStatus());
         }
         model.addAttribute("ratings", ratingRepository.findAll());
         return "rating/list";
