854ebf0a4c5906cc6cba7f833ca88822db8dea53
jedi10 <kyle10_dev@hotmail.fr>
2021-03-22 18:22:18 +0100

    Add Logger in BidList Controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -2,6 +2,7 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +10,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
-
+@Slf4j
 @Controller
 @RequestMapping("/bidList/")
 public class BidListController {
@@ -24,37 +29,60 @@ public class BidListController {
     }
 
     @RequestMapping(value = "list", method = RequestMethod.GET)
-    public String home(Model model)
+    public String home(Model model, HttpServletRequest request, HttpServletResponse response)
     {
+        log.info("Get bid List on URI: '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                response.getStatus());
         model.addAttribute("bids", bidListRepository.findAll());
         return "bidList/list";
     }
 
     @GetMapping(value = "add")
-    public String addBidForm(BidList bid, Model model) {
+    public String addBidForm(BidList bid, Model model,
+            HttpServletRequest request, HttpServletResponse response) {
         model.addAttribute("bid", new BidList());
+        log.info("Go to Creation Bid Form on URI: '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                response.getStatus());
         return "bidList/add";
     }
 
     @PostMapping(value = "validate", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE+";charset=UTF-8"})//"application/x-www-form-urlencoded")
     public String validate(@Valid  @ModelAttribute("bid") BidList bid,
                            BindingResult result,
-                           Model model) {
+                           Model model,
+                           HttpServletRequest request, HttpServletResponse response) {
         if (result.hasErrors()) {
+            log.warn("Bid Creation Error on URI: '{}': Error Field(s): '{}' : RESPONSE STATUS: '{}'",
+                    request.getRequestURI(),
+                    result.getFieldErrors().stream()
+                            .map(e-> e.getField().toUpperCase())
+                            .distinct()
+                            .collect(Collectors.joining(", ")),
+                    response.getStatus());
             return "bidList/add";
         }
-        bidListRepository.save(bid);
+        BidList bidCreated = bidListRepository.save(bid);
+        log.info("Bid Creation on URI: '{}' : Bid Created '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                bidCreated.getId() + " " + bidCreated.getAccount(),
+                response.getStatus());
         model.addAttribute("bids", bidListRepository.findAll());
         return "bidList/list";
     }
 
     @GetMapping(value = "update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id,
-                                 Model model) {
+                                 Model model,
+                                 HttpServletRequest request, HttpServletResponse response) {
         String errorMessage = "Invalid bid Id: there is no bid with Id: ";
 
         BidList bid = bidListRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(errorMessage + id));
+        log.info("Go to Update Bid Form on URI: '{}': RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                response.getStatus());
         model.addAttribute("bid", bid);
         return "bidList/update";
     }
@@ -62,24 +90,44 @@ public class BidListController {
     @PostMapping(value = "update/{id}")
     public String updateBid(@PathVariable("id") Integer id,
                             @Valid  @ModelAttribute("bid") BidList bidList,
-                            BindingResult result, Model model) {
+                            BindingResult result, Model model,
+                            HttpServletRequest request, HttpServletResponse response) {
         if (result.hasErrors()) {
+            log.warn("Update Bid Error on URI: '{}': Error Field(s):'{}' : RESPONSE STATUS: '{}'",
+                    request.getRequestURI(),
+                    result.getFieldErrors().stream()
+                            .map(e-> e.getField().toUpperCase())
+                            .distinct()
+                            .collect(Collectors.joining(", ")),
+                    response.getStatus());
             return "bidList/update";
         }
-
         bidList.setId(id);
-        bidListRepository.save(bidList);
+        BidList bidUpdated = bidListRepository.save(bidList);
+        log.info("Bid Update on URI: '{}' : Bid Updated '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                bidUpdated.getId() + " " + bidUpdated.getAccount(),
+                response.getStatus());
         model.addAttribute("bids", bidListRepository.findAll());
         return "bidList/list";
     }
 
     @RolesAllowed("ADMIN")
     @GetMapping(value = "delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
+    public String deleteBid(@PathVariable("id") Integer id, Model model,
+                            HttpServletRequest request, HttpServletResponse response) {
         Optional<BidList> bidOptional = bidListRepository.findById(id);
         if(bidOptional.isPresent()){
             bidListRepository.deleteById(id);
+            log.info("Delete Bid on URI: '{}' : RESPONSE STATUS: '{}'",
+                    request.getRequestURI(),
+                    response.getStatus());
+        } else {
+            log.warn("No Bid was deleted on URI: '{}' : RESPONSE STATUS: '{}'",
+                    request.getRequestURI(),
+                    response.getStatus());
         }
+
         model.addAttribute("bids", bidListRepository.findAll());
         return "bidList/list";
     }
