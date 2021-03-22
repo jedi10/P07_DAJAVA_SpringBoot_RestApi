674ba713d486cbb3f1b3c61e5b486ca22255014a
jedi10 <kyle10_dev@hotmail.fr>
2021-03-22 17:57:10 +0100

    Add User Controller Logger

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -3,19 +3,26 @@ package com.nnk.springboot.controllers;
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.service.IUserDalService;
 import com.nnk.springboot.web.dto.UserDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Collection;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user/")
 public class UserController {
+
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private IUserDalService userService;
 
@@ -24,37 +31,63 @@ public class UserController {
     }
 
     @RequestMapping(value = "list", method = RequestMethod.GET)
-    public String home(Model model)
+    public String home(Model model, HttpServletRequest request, HttpServletResponse response)
     {
         Collection<UserDTO> userList =  userService.findAll();
         model.addAttribute("users", userList);
+        logger.info("Get User List on URI: '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                response.getStatus());
         return "user/list";
     }
 
     @GetMapping(value = "add")
-    public String addUser(UserDTO user, Model model) {
+    public String addUser(UserDTO user, Model model,
+                          HttpServletRequest request, HttpServletResponse response) {
         model.addAttribute("user", new UserDTO());
+        logger.info("Go to Creation User Form on URI: '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                response.getStatus());
         return "user/add";
     }
 
     @PostMapping(value = "validate")
     public String validate(@Valid @ModelAttribute("user") UserDTO user,
                            BindingResult result,
-                           Model model) {
-        if (!result.hasErrors()) {
-            userService.create(user);
-            model.addAttribute("users", userService.findAll());
-            return "user/list";
+                           Model model,
+                           HttpServletRequest request, HttpServletResponse response) {
+        if (result.hasErrors()) {
+            logger.warn("User Creation Error on URI: '{}': Error Field(s): '{}' : RESPONSE STATUS: '{}'",
+                    request.getRequestURI(),
+                    result.getFieldErrors().stream()
+                            .map(e-> e.getField().toUpperCase())
+                            .distinct()
+                            .collect(Collectors.joining(", ")),
+                    response.getStatus());
+            return "user/add";
         }
-        return "user/add";
+        User userCreated = userService.create(user);
+        StringJoiner joinUserName = new StringJoiner(" ");
+        joinUserName.add(userCreated.getId().toString());
+        joinUserName.add(userCreated.getFullname());
+        logger.info("User Creation on URI: '{}' : User Created '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                joinUserName,
+                response.getStatus());
+        model.addAttribute("users", userService.findAll());
+        return "user/list";
     }
 
     @GetMapping(value = "update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
+    public String showUpdateForm(@PathVariable("id") Integer id, Model model,
+                                 HttpServletRequest request, HttpServletResponse response) {
         UserDTO user = userService.findOne(id);
         user.setPassword("");
         model.addAttribute("user", user);
         model.addAttribute("id", id);
+        logger.info("Go to Update User Form on URI: '{}': RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                response.getStatus());
         return "user/update";
     }
 
@@ -62,11 +95,26 @@ public class UserController {
     public String updateUser(@PathVariable("id") Integer id,
                              @Valid @ModelAttribute("user") UserDTO user,
                              BindingResult result,
-                             Model model) {
+                             Model model,
+                             HttpServletRequest request, HttpServletResponse response) {
         if (result.hasErrors()) {
+            logger.warn("Update User Error on URI: '{}': Error Field(s):'{}' : RESPONSE STATUS: '{}'",
+                    request.getRequestURI(),
+                    result.getFieldErrors().stream()
+                            .map(e-> e.getField().toUpperCase())
+                            .distinct()
+                            .collect(Collectors.joining(", ")),
+                    response.getStatus());
             return "user/update";
         }
-        userService.update(user, id);
+        User userUpdated = userService.update(user, id);
+        StringJoiner joinUserName = new StringJoiner(" ");
+        joinUserName.add(userUpdated.getId().toString());
+        joinUserName.add(userUpdated.getFullname());
+        logger.info("Update User on URI: '{}' : User Updated: '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                joinUserName,
+                response.getStatus());
         model.addAttribute("users", userService.findAll());
         return "user/list";
     }
@@ -74,9 +122,13 @@ public class UserController {
     @RolesAllowed("ADMIN")
     //@PreAuthorize("hasRole('ADMIN')")
     @GetMapping(value = "delete/{id}")
-    public String deleteUser(@PathVariable("id") Integer id, Model model) {
+    public String deleteUser(@PathVariable("id") Integer id, Model model,
+                             HttpServletRequest request, HttpServletResponse response) {
         userService.delete(id);
         model.addAttribute("users", userService.findAll());
+        logger.info("Delete User on URI: '{}' : RESPONSE STATUS: '{}'",
+                request.getRequestURI(),
+                response.getStatus());
         return "user/list";
     }
 }
@@ -88,3 +140,7 @@ public class UserController {
 //https://stackabuse.com/spring-boot-thymeleaf-form-data-validation-with-bean-validator/
 
 //https://mkyong.com/spring-boot/spring-rest-spring-security-example/
+
+//Logging https://stackoverflow.com/questions/37710557/how-to-get-request-url-in-spring-boot-restcontroller
+
+//List to string with join comma (lamda), https://dzone.com/articles/java-8-convert-list-to-string-comma-separated
